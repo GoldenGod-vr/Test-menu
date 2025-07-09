3fff70ae08fe1aec53b10c9c4b977fe6b7e92f35
GoldenGod-vr <tc.kchanda21@gmail.com>
2025-07-08 18:09:06 -0800

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.GoldenGod.gorillatag.menutemplate";
+        public const string Name = "TEST";
+        public const string Description = "Created by GOLDENGOD with love <3";
+        public const string Version = "1.0";
+    }
+}
